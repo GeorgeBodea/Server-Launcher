e09efa0fe8e4423ae09da31592f1f19247260294
GeorgeBodea <georgebodeacn@gmail.com>
2024-03-03 17:07:03 +0200

    Added check/create config feature
    In the event that the config.py file does not exist, one will be created before launching the application.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-from website import create_app
+from website import create_app, ensure_config_presence
 
-app = create_app()
 
 if __name__ == '__main__':
+    ensure_config_presence()
+    
+    app = create_app()
     app.run(debug=True, host="0.0.0.0", port=5000)
--- src/website/__init__.py
+++ src/website/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask 
 from .views import views
 from .auth import auth
+from pathlib import Path
+from secrets import token_hex
 
 def create_app():
     app = Flask(__name__)
@@ -10,3 +12,19 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
 
     return app
+
+def ensure_config_presence():
+    current_folder = Path(__file__).parent.absolute()
+    config_abs_path = current_folder / "config.py"
+
+    if config_abs_path.is_file():
+        pass 
+    else:
+        secret_key = token_hex()
+        file_content = "SECRET_APP=\'{}\'".format(secret_key)
+
+        with open(config_abs_path, 'w') as file_config:
+            file_config.write(file_content)
+        
+        print("Config file has been auto-generated.")
+        
