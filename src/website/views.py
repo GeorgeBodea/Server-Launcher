9c3d31b0a2fe1f32e6b1bb800a5691b84e3d8358
GeorgeBodea <georgebodeacn@gmail.com>
2024-04-21 15:37:29 +0300

    Added mock instance selector
    This will be improved to support actual cloud instances launching.

--- src/website/views.py
+++ src/website/views.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    if request.method == "POST":
+        instance_type = request.form.get('selectedOptionInstanceType')
+        print(instance_type)
     return render_template("home.html", user=current_user)
